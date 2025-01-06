da5417185a5b059f7feed2d201a53338e7e6f2cb
erfajc97 <erfacrypto@gmail.com>
2025-01-06 01:29:57 -0500

    server conection

--- prisma/migrations/20250106062529_init/migration.sql
+++ prisma/migrations/20250106062529_init/migration.sql
@@ -2,6 +2,9 @@
 CREATE TABLE "Post" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL DEFAULT 'Anonymous',
+    "tags" TEXT NOT NULL DEFAULT 'tecnology',
+    "timeToRead" INTEGER NOT NULL DEFAULT 5,
     "image" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
